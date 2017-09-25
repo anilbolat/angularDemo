9a9b450204efd665cdb4c04939af5499fae6eb81
Anil Bolat <anilbolat90@gmail.com>
2017-09-25 20:38:12 +0300

    read json file and print console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product';
+import { ProductService } from './products.service';
+import { appService } from './app.service';
+import { Http , Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+  template: '<div>Hello!</div>',
+  providers: [ProductService]
 })
+
 export class AppComponent  { 
-	appTitle: string = 'WelcomeYOOO';
-	appStatus: boolean = true;
-	appList: any[] = [
-	{
-		"Id": "1",
-		"Name": "tarik"
-	},
-	{
-		"Id": "2",
-		"Name": "elmander"
+	iproducts: IProduct[];
+	costructor(private _product: ProductService) {
 	}
-	];
+
+	ngOnInit(): void {
+		this._product.getproducts()
+		.subscribe(iproducts => this.iproducts = iproducts);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent }  from './app.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports:      [ BrowserModule ],
