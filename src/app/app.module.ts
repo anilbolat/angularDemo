5af89e98aa8034bfc4710c146d4cadb331cace3a
Anil Bolat <anilbolat90@gmail.com>
2017-09-26 16:02:21 +0300

    routing 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent }  from './app.component';
 import { AppInventory } from './Inventory.component';
 import { Appproduct } from './product.component';
+import { PageNotFoundComponent } from './NotFound.component';
 
 const appRoutes: Routes = [
 { path: 'Product', component: Appproduct },
 { path: 'Inventory', component: AppInventory },
+{ path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
 	imports:      [ BrowserModule, RouterModule.forRoot(appRoutes) ],
-	declarations: [ AppComponent,AppInventory,Appproduct ],
+	declarations: [ AppComponent,AppInventory,Appproduct,PageNotFoundComponent ],
 	bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
