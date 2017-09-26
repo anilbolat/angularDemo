d698d41cd665b0ae229006cfe62e0683a9d57e35
Anil Bolat <anilbolat90@gmail.com>
2017-09-26 15:54:41 +0300

    routing to pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
 import { Component } from '@angular/core';
-import { IProduct } from './products';
-import { ProductService } from './products.service';
-//import { appService } from './app.service';
-import { Http , Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'my-app',
-  template: '<div>Hello!</div>',
-  providers: [ProductService]
+	selector: 'my-app',
+	templateUrl : 'app/app.component.html'
 })
 
 export class AppComponent  { 
-	iproducts: IProduct[];
-	constructor(private _product: ProductService) {
-	}
-
-	ngOnInit(): void {
-		this._product.getproducts()
-		.subscribe(iproducts => this.iproducts = iproducts);
-	}
+	
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent }  from './app.component';
-import { HttpModule } from '@angular/http';
+import { AppInventory } from './Inventory.component';
+import { Appproduct } from './product.component';
+
+const appRoutes: Routes = [
+{ path: 'Product', component: Appproduct },
+{ path: 'Inventory', component: AppInventory },
+];
 
 @NgModule({
-  imports:      [ BrowserModule, HttpModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+	imports:      [ BrowserModule, RouterModule.forRoot(appRoutes) ],
+	declarations: [ AppComponent,AppInventory,Appproduct ],
+	bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
