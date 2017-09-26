ca90640a71e1f2799053f016771a9bcc7eb819c9
Anil Bolat <anilbolat90@gmail.com>
2017-09-26 16:16:10 +0300

    navigation

---
+++ src/app/Inventory.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Router }  from '@angular/router'; 
+
+@Component ({
+	selector: 'my-app',
+	templateUrl: 'app/Inventory.component.html',
+})
+export class AppInventory  {
+	constructor(private _router: Router){} 
+
+	onBack(): void { 
+		this._router.navigate(['/Product']); 
+	} 
+}
---
+++ src/app/NotFound.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component ({  
+	selector: 'my-app',  
+	template: '404 Not Found', 
+})  
+export class PageNotFoundComponent {  
+} 
