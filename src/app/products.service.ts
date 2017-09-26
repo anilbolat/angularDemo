23c515ad334d32694a8aa835ee12381ae25e0876
Anil Bolat <anilbolat90@gmail.com>
2017-09-26 15:00:37 +0300

    add catch for error handling

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -3,6 +3,7 @@ import { Http , Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
 import { IProduct } from './products';
 
 @Injectable()
@@ -13,6 +14,12 @@ export class ProductService {
    getproducts(): Observable<IProduct[]> {
       return this._http.get(this._producturl)
       .map((response: Response) => <IProduct[]> response.json())
-      .do(data => console.log(JSON.stringify(data)));
+      .do(data => console.log(JSON.stringify(data)))
+      .catch(this.handleError);
+   }
+
+   private handleError(error: Response) {
+   	console.error(Response);
+   	return Observable.throw(error.json().error());
    }
 }
